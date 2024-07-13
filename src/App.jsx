ca5a08adeee565a029a8f57a65c8f26c4e7e87d8
AlbaHoyo <alba.maria.hoyo@gmail.com>
2024-07-13 16:51:09 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removeTodo } from './redux/todosSlice';
+
 const App = () => {
-  
+  const [task, setTask] = useState('');
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTask = () => {
+    if (task.trim() !== '') {
+      dispatch(addTodo(task));
+      setTask('');
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    dispatch(removeTodo(index));
+  };
+
   return (
-    <></>
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Agregar Tarea</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => handleRemoveTask(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
---
+++ src/redux/store.jsx
@@ -0,0 +1,30 @@
+
+import { createStore } from 'redux';
+
+
+const initialState = {
+  todos: []
+};
+
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      };
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo, index) => index !== action.payload)
+      };
+    default:
+      return state;
+  }
+};
+
+
+const store = createStore(todosReducer);
+
+export default store;
